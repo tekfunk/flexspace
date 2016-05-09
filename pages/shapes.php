ac02ba18361abba2ff653f2aa99bf967d8b01b25
Rex Wesley Reyes <slee@sleesbook.local>
2016-05-09 14:01:26 -0700

    Minor Stuff
    I did while around town.  Added shapes, worked on layout.

---
+++ pages/shapes.php
@@ -0,0 +1,7 @@
+<?php include("header.html"); ?>
+<section class="section">
+	<div class="container">
+    	<h1 class="title">Shapes</h1>
+		<h2 class="subtitle">Drawing with CSS</h2>
+	</div>
+</section><?php include("footer.html"); ?>
