e3eb6496889a1c5e813c85739d8c96ec47bd01c5
Rex Wesley Reyes <rwr@casadereyes.net>
2016-06-15 13:05:00 -0700

    Added Mobile Nav
    Used the new toggle component.

--- inc/header.php
+++ inc/header.php
@@ -3,6 +3,25 @@
     <div class="nav-left">
       <div class="logo"><a href="/index.php"><img src="../art/flexspace-logo-sm.svg" width="80"></a></div>
     </div>
+      <span class="nav-item nav-toggle">
+ <a href class="toggle-btn on-right on-win-edge"><i class="ics icon-list"></i></a>
+<div id="toggle-window" class="toggle-window is-primary on-win-edge">				
+  <a href class="toggle-btn-close on-right on-win-edge"><i class="ics icon-ex"></i></a>
+  
+      <div class="block" style="width:30%;padding:4px;"> <a class="button is-fullwidth is-primary is-inverted" href="/page.php">page</a> <a class="button is-primary" href="/page.php#layout">layout</a><a class="button is-primary" href="/page.php#container">container</a> <a class="button is-primary" href="/page.php#sizes">sizes</a><a class="button is-primary" href="/page.php#hero">hero</a>  <a class="button is-primary" href="/page.php#helpers">helpers</a> <a class="button is-primary" href="/page.php#display">display</a> 
+       </div>
+      <div class="block" style="width:30%;padding:4px;"> <a class="button is-fullwidth is-primary is-inverted" href="/copy.php">copy</a> <a class="button is-primary" href="/copy.php#content">content</a> <a class="button is-primary" href="/copy.php#badges">badges</a> <a class="button is-primary" href="/copy.php#burst">burst</a> <a class="button is-primary" href="/copy.php#messages">messages</a> <a class="button is-primary" href="/copy.php#notifications">notifications</a> <a class="button is-primary" href="/copy.php#code">code</a> </div>
+      
+   
+      <div class="block" style="width:30%;padding:4px;"> <a class="button is-fullwidth is-primary is-inverted" href="/grid.php">grid</a> <a class="button is-primary is-active" href="/grid.php#columns">columns</a><a class="button is-primary" href="/grid.php#sizing">sizing</a><a class="button is-primary is-disabled" href="/grid.php#flex">flex</a><a class="button is-primary" href="/grid.php#isotope">isotope</a><a class="button is-primary" href="/grid.php#tables">tables</a> </div>
+      <div class="block" style="width:30%;padding:4px;"> <a class="button is-fullwidth is-primary is-inverted" href="/modules.php">modules</a><a class="button is-primary" href="/modules.php#box">box</a> <a class="button is-primary" href="/modules.php#media">media</a> <a class="button is-primary" href="/modules.php#mod">mod</a><a class="button is-primary is-disabled" href="/modules.php#gallery">gallery</a> <a class="button is-primary is-disabled" href="/modules.php#modal">modal</a></div>
+      <div class="block" style="width:30%;padding:4px;"> <a class="button is-fullwidth is-primary is-inverted" href="/ui.php">ui</a> <a class="button is-primary" href="/ui.php#button">button</a><a class="button is-primary" href="/ui.php#nav">nav</a><a class="button is-primary is-disabled" href="/ui.php#tabs">tabs</a><a class="button is-primary is-disabled" href="/ui.php#panel">panel</a><a class="button is-primary is-disabled" href="/ui.php#menu">menu</a><a  class="button is-primary is-disabled" href="/ui.php#form">form</a><a class="button is-primary is-disabled" href="/ui.php#pagination">pagination</a></div>
+      <div class="block" style="width:30%;padding:4px;"> <a class="button is-fullwidth is-primary is-inverted" href="/art.php">art</a><a class="button is-primary" href="/art.php#Palette">palette</a> <a class="button is-primary is-disabled" href="/art.php#logos">logos</a> <a class="button is-primary" href="/art.php#icons">icons</a> <a class="button is-primary" href="/art.php#backgrounds">backgrounds</a></div>
+      <div class="block" style="width:30%;padding:4px;"> <a class="button is-fullwidth is-primary is-inverted" href="/animations.php">animations</a><a class="button is-primary" href="/ui.php#loaders">loaders</a><a class="button is-primary" href="/ui.php#progress">progress</a><a class="button is-primary" href="/animations.php#animate">animate.css</a> </div>
+       <div class="block" style="width:30%;padding:4px;"> <a class="button is-fullwidth is-primary is-inverted" href="/variables.php">variables</a> <a class="button is-primary" href="/variables.php#Colors">colors</a> <a class="button is-primary" href="/variables.php#Type">type</a> <a class="button is-primary" href="/variables.php#Layout">layout</a> </div>
+    </div>
+  </span>
+
     <div class="nav-right nav-menu"> 
     	<span class="nav-item"><a href="/page.php">page</a></span> 
 	    <span class="nav-item"><a href="/copy.php">copy</a></span>
